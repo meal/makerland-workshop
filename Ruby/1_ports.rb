436549a04766840eaa0331f4a6b6f5e2f58af614
Mateusz Kozak <mateusz@mkozak.pl>
2014-02-06 22:11:08 +0100

    yay, missin file added

---
+++ Ruby/1_ports.rb
@@ -0,0 +1,32 @@
+require 'serialport'
+
+def list_serial_ports
+  if Gem.win_platform? == true
+    availables = []
+    (1..256).each do |p|
+      begin
+        ser = SerialPort.new(p)
+        availables << "COM#{p}"
+      rescue Errno::ENOENT => e
+        File.open("ports.log", 'wb') do |f|
+          f << 'could not open file'
+        end
+      end
+    end
+    availables.each do |available|
+      puts "\t #{available}"
+    end
+  else
+    Dir.glob("/dev/tty.*") do |port|
+      puts "\t #{port}"
+    end
+  end
+end
+
+if __FILE__ == $0
+  puts ""
+  puts "Available ports:"
+  puts ""
+  list_serial_ports
+  puts ""
+end
