c4ae660397a28b9a93b9048aec7e2e815c326985
Wojtek Siudzinski <admin@suda.pl>
2014-02-06 22:04:16 +0100

    Added sonar

---
+++ Python/3_sonar.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import time
+import sys
+from serial import Serial
+
+def main():
+    if len(sys.argv) != 2:
+    	print "usage: python %s <serial>" % __file__
+        sys.exit(2)
+
+    serial = Serial(sys.argv[1])
+
+    while True:
+        serial.write("S")
+        distance = serial.readline().strip()
+        sys.stdout.write("Distance is %scm     \r" % distance)
+        sys.stdout.flush()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
